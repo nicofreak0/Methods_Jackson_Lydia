a41b4ff5fa6f377443b7b2c563be209873ec4c58
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-03-13 14:34:33 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Methods_Jackson_Lydia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Multiply(6,8));
+        }
+
+        static int Multiply(int num1, int num2)
+        {
+                return num1 * num2;
+        }
+
+    }
+}
