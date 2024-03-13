4ace7b55857d31e88e1e05311aeadc5e4c8c5f6d
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-03-13 14:52:46 -0500

    f

--- Program.cs
+++ Program.cs
@@ -4,18 +4,51 @@
     {
         static void Main(string[] args)
         {
+            // asks user to enter whole number and prints to console
             Console.WriteLine("Enter a whole number.");
             int x = Convert.ToInt32(Console.ReadLine());
+            // asks user to enter another whole number and prints to console
             Console.WriteLine("Enter another a whole number.");
             int y = Convert.ToInt32(Console.ReadLine());
 
+            // prints multiplied numbers to console
             Console.WriteLine(Multiply(x,y));
+
+            // asks user to enter name and prints it to console
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            WelcomeUser(name);
+
+            Goodbye();
         }
 
+        /// <summary>
+        /// multiplies num1 and num2 together
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
         static int Multiply(int num1, int num2)
         {
                 return num1 * num2;
         }
 
+        /// <summary>
+        /// prints out welcome message to console
+        /// </summary>
+        /// <param name="username"></param>
+        static void WelcomeUser(string username) 
+        {
+                Console.WriteLine($"Hey, {username} glad you're here.");
+        }
+
+        /// <summary>
+        /// prints a goodbye method to console
+        /// </summary>
+        static void Goodbye()
+        {
+            Console.WriteLine("Have a nice day.");
+        }
     }
 }
